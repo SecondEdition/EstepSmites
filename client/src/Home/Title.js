c9188290a3b663c567933e30dff76e39d530e2a4
SecondEdition <maxwellfth@gmail.com>
2020-05-22 15:57:34 -0600

    setup clientside

--- client/src/Home/Title.js
+++ client/src/Home/Title.js
@@ -1,14 +1,17 @@
 import React, { Fragment } from 'react';
-import Title from './Home/Title';
-
-
 // from: https://react-bootstrap.github.io/getting-started/introduction
 import 'bootstrap/dist/css/bootstrap.min.css'; // <--- what does including this do, really? 
 
 function App() {
   return (
     <Fragment> 
-      <Title />
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This is a title?
+        </p>
+      </header>
+    </div>
     </Fragment>
   );
 }
